eaff7155cdedfbbce5ee0e61bd7a3fe60c04a057
etkaesque <edga.majo@gmail.com>
2023-07-01 00:39:55 +0300

    update

--- src/pages/ask/index.tsx
+++ src/pages/ask/index.tsx
@@ -1,14 +1,48 @@
 import Header from "../../components/Header";
-import styles from "./Home.module.css";
+import styles from "./index.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { space } from "postcss/lib/list";
 
 export default function Home() {
 
+    const [verified, setVerified] = useState(false)
+    const [token, setToken] = useState<string | null>(null); // token 
+
+    useEffect(() => {
+      let localToken = localStorage.getItem("jwt_token");
+      if(localToken) {
+        console.log("setting a token", localToken)
+        setToken(localToken)
+      }
+    }, []);
+    
+    useEffect(() => {
+      const verify = async () => {
+        if(token) {
+          console.log("try token", token)
+          try {
+            const response = await axios.post("http://localhost:8080/verifyToken", {}, {headers: {
+              authorization: token
+            }})
+            setVerified(true)
+          } catch (error) {
+            console.log(error)
+            setVerified(false)
+          }
+        }
+      } 
+      verify()
+    }, [token]);
+
+    // if token valid display page if not display: Only signin in users can post a question, click astronaut to log in.
+
     const router = useRouter()
     const [formData, setFormData] = useState({
+      title: "",
+      content: ""
 
     });
   
@@ -31,33 +65,58 @@ export default function Home() {
   
       try {
         const response = await axios.post(
-          "http://localhost:8080/",
-          formData
-        );
-  
-        if (response) {
-          router.push('/');
-        }
+          "http://localhost:8080/askQuestion",
+          formData, 
+          {headers: 
+            {
+            authorization: token
+            }
+          })
+        
+
+        router.push('/');
+        
   
       } catch (error) {
         console.log("Failed to save data to database, error: ", error);
       }
     }
 
-
+    console.log(verified)
   return (
     <div className="background">
       <Header />
 
       <main className="flex justify-center ">
-        <section className=" relative max-w-7xl mx-10">
-          <div className="sectionStyles">
-            
-            <form action="submit">
-                <label htmlFor="title">Title</label>
-                <textarea name="question">Question</textarea>
+      
+        <section className=" sectionStyles relative max-w-7xl  w-3/4  mx-10">
+
+          {verified ? (
+          <>
+          
+          <h1 className="text-2xl mb-3 ">Post A Question</h1>
+
+         
+            <form action="submit" onSubmit={handleSumbit} className="flex flex-col gap-3">
+  
+                <input placeholder="Title" onChange={handleChange} className={styles.title}  name="title" value={formData.title} maxLength={50} required />
+
+                <textarea placeholder="Question" onChange={handleChange} className={styles.content}  name="content" value={formData.content} maxLength={1000} required></textarea>
+
+                <div className="flex justify-end">
+
+                <button type="submit" className="text-white mainColorBackground hover:opacity-70 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-auto sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Post
+                </button>
+
+                </div>
+                
+
             </form>
-          </div>
+
+          </>) : (<><h1>Only registered user can post questions.</h1></>)}
+
+         
         </section>
       </main>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,58 @@
 import Header from "../components/Header";
 import styles from "./Home.module.css";
 import Link from "next/link";
+import axios from "axios";
 
-export default function Home() {
+type questionType = {
+  title: string;
+  content: string;
+  date_created: Date;
+  author: String;
+  authorIdObect: {};
+};
+
+export default function Home({ data, count }: { data: questionType[] }) {
   return (
     <div className="background">
       <Header />
 
-      <main className="flex justify-center ">
-        <section className=" relative max-w-7xl mx-10">
-          <div className="sectionStyles">
-            <Link
-              href={"/ask"}
-              className={styles.button}
-            >
-              Post a question
+      <main className="flex flex-col items-center gap-y-4">
+        <section className=" relative max-w-7xl w-3/4 mx-10 ">
+          <div className="sectionStyles flex justify-center items-center gap-x-2">
+            <Link href={"/ask"} className={styles.button}>
+              Ask here
             </Link>
-            <br />
-            <span>241 questions</span>
-            <br />
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit ipsum
-            voluptates ipsa explicabo eum eveniet ad fugit aperiam dicta
-            expedita neque, sed temporibus non facere rem? Optio totam facere
-            repellendus?
+
+            <span>or browse all {count} questions</span>
           </div>
         </section>
+
+        {data &&
+          data.map((question) => {
+            return (
+              <article className="sectionStyles2 relative max-w-7xl w-3/4 mx-10">
+                <Link className="questionStyle" href={"/"}>
+                  <span className="questionAuthor">
+                    Asked by {question.authorIdObect.name} on{" "}
+                    {question.date_created.toString().split("T")[0]}
+                  </span>
+                  <h2 className="questionh2">{question.title}</h2>
+                  <p className="questionContent">{question.content}</p>
+                </Link>
+              </article>
+            );
+          })}
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await axios.get("http://localhost:8080/allQuestions");
+  const data = response.data.questions;
+  const count = response.data.questionCount;
+
+  console.log("count is ", count);
+
+  return { props: { data, count } };
 }
